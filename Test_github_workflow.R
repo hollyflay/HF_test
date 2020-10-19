297de2d91879f6d6764703d66d7354b0b5e2c8e3
hollyflay <holly.flay@dairynz.co.nz>
2020-10-20 11:27:00 +1300

    Second commit

--- Test_github_workflow.R
+++ Test_github_workflow.R
@@ -1,6 +1,16 @@
 # Test script to demonstrate git workflow
+
+# install.packages("usethis")
 library(usethis)
 use_git_config(user.name = "hollyflay", user.email = "holly.flay@dairynz.co.nz")
 
+
 usethis::use_git()
+#Commit git
+
+usethis::use_github()
+
+#Personal access token
+usethis::edit_r_environ()
+#In .Renviron; GITHUB_PAT=xxxPATxxx
 
